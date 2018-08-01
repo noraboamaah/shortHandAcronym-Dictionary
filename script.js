36bff3d7954394b37793e5beb8ae83a17ce60795
Nora Mensah <noraboamaah@gmail.com>
2018-08-01 11:22:33 -0700

    finish functionality

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 var outputAreaRef = document.querySelector(".outputArea");
+var button = document.querySelector(".btnSearch");
 	var output = " ";
 
 
 
-document.querySelector(".btnSearch").addEventListener("click", function(word){
-	
-		var word = document.querySelector(".word");
+button.addEventListener("click", search); 
 
-	var accronyms = {
-	omg: "Oh My God",
-	lol: "Laugh Out Loud",
-	lmao: "Laugh My Ass Off",
-	wtf: "What The F**k"
-	};
+	function search(){
+	
+		var word = document.querySelector(".word").value;
+
+				var accronyms = {
+				omg: "Oh My God",
+				lol: "Laugh Out Loud",
+				lmao: "Laugh My Ass Off",
+				wtf: "What The F**k",
+				rn: "Right Now", 
+				nvm: "Never Mind",
+				tb: "throw back",
+				tbt: "throw back thursday",
+				mcm: "Man Cruch Monday",
+				mcw: "Man Cruch Wednesday",
+				mce: "Man Crush Everyday",
+				wcm: "Woman Crush Monday",
+				wce: "Woman Crush Everyday",
+				// s/o: "Shout out",
+				tbh: "To be Honest",
+				brb: "Be Right back",
+				wyd: "What You Doing",
+				wydrn: "What you Doing Right Now",
+				kmft: "Kommot For There",
+				fk: "F**k You",
+				smh: "Shaking My Head",
+				smhvh: "Shaking My Head Very Hard",
+				bs: "bullshit"
+			};
 
 if (accronyms[word] !== undefined){
 	word = accronyms[word];
@@ -21,4 +43,4 @@ if (accronyms[word] !== undefined){
 output = word;
 
 outputAreaRef.innerHTML = output;
-});
+};
